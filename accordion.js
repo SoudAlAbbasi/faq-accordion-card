68c8c6feae5255bf26c337b4e47c2a1f1246eda5
Dante <126198011+SoudAlAbbasi@users.noreply.github.com>
2023-08-06 21:01:50 +0300

    update

--- accordion.js
+++ accordion.js
@@ -4,11 +4,11 @@ var i;
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var answer = this.nextElementSibling;
-    if (answer.style.maxHeight) {
-      answer.style.maxHeight = null;
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
     } else {
-      answer.style.maxHeight = answer.scrollHeight + "px";
+      panel.style.maxHeight = panel.scrollHeight + "px";
     } 
   });
 }
